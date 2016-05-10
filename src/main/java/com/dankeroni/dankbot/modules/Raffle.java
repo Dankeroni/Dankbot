d293ac417ebb56a41c91aa393fe1d20e5ea2130e
dankeroni <pana99@hotmail.de>
2016-05-10 15:51:57 +0200

    Fixed raffle bug

--- src/main/java/com/dankeroni/dankbot/modules/Raffle.java
+++ src/main/java/com/dankeroni/dankbot/modules/Raffle.java
@@ -27,12 +27,17 @@ public class Raffle extends Module {
         if (raffleRunning) return;
 
         String[] messageArgs = Utils.makeArgs(message);
+        if (messageArgs.length == 0) return;
         int rafflePoints, raffleTime;
 
         try {
             rafflePoints = Integer.parseInt(messageArgs[0]);
-            raffleTime = Integer.parseInt(messageArgs[1]);
-        } catch (ArrayIndexOutOfBoundsException e) {
+            try {
+                raffleTime = messageArgs.length > 1 ? Integer.parseInt(messageArgs[1]) : 60;
+            } catch (NumberFormatException ignored) {
+                raffleTime = 60;
+            }
+        } catch (NumberFormatException e) {
             return;
         }
 
@@ -40,9 +45,10 @@ public class Raffle extends Module {
         enteredUsers = new ArrayList<>();
         bot.channelMessage("A raffle has begun for " + rafflePoints + " points. Type !join to join the raffle! The raffle will end in " + raffleTime + " seconds");
         long sleepTime = (long) (raffleTime * 1000 * 0.25);
-        Utils.runDelayed(() -> bot.channelMessage("The raffle for " + rafflePoints + " points ends in " + (int) (raffleTime * 0.75) + " seconds! Type !join to join the raffle!"), sleepTime);
-        Utils.runDelayed(() -> bot.channelMessage("The raffle for " + rafflePoints + " points ends in " + (int) (raffleTime * 0.50) + " seconds! Type !join to join the raffle!"), sleepTime * 2);
-        Utils.runDelayed(() -> bot.channelMessage("The raffle for " + rafflePoints + " points ends in " + (int) (raffleTime * 0.25) + " seconds! Type !join to join the raffle!"), sleepTime * 3);
+        int finalRaffleTime = raffleTime;
+        Utils.runDelayed(() -> bot.channelMessage("The raffle for " + rafflePoints + " points ends in " + (int) (finalRaffleTime * 0.75) + " seconds! Type !join to join the raffle!"), sleepTime);
+        Utils.runDelayed(() -> bot.channelMessage("The raffle for " + rafflePoints + " points ends in " + (int) (finalRaffleTime * 0.50) + " seconds! Type !join to join the raffle!"), sleepTime * 2);
+        Utils.runDelayed(() -> bot.channelMessage("The raffle for " + rafflePoints + " points ends in " + (int) (finalRaffleTime * 0.25) + " seconds! Type !join to join the raffle!"), sleepTime * 3);
         Utils.runDelayed(() -> {
             if (enteredUsers.size() > 0) {
                 String winner = enteredUsers.get(random.nextInt(enteredUsers.size()));
